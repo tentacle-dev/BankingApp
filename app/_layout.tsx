99e7a54bbf18696751c88303cc9a476ca033f4e3
tentacle-dev <tanush0525@gmail.com>
2025-02-07 10:42:15 -0500

    Nativewind and Tailwind setup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -8,6 +8,7 @@ import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
 
+import "../global.css";
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
