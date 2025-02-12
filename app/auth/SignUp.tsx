702a77ae1028d772bb8f3c45cc00af41fb0127a8
GeorgeThomas777 <george.velamkunnel@gmail.com>
2025-02-12 14:31:49 -0500

    add some more logic

--- app/auth/SignUp.tsx
+++ app/auth/SignUp.tsx
@@ -1,20 +1,31 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Stack } from 'expo-router';
 import { View, Text, TextInput, TouchableOpacity, Alert, Image } from "react-native";
 import { CheckSquare, Square } from "lucide-react-native";
-import { green } from "react-native-reanimated/lib/typescript/Colors";
+import CountryPicker, { Country, CountryCode } from "react-native-country-picker-modal";
 
 const SignUp: React.FC = () => {
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
+  const [phoneno, setPhoneno] = useState<string>("");
   const [checked, setChecked] = useState(false);
+  const [isDisabled, setIsDisabled] = useState(true);
+
+  const [countryCode, setCountryCode] = useState<CountryCode>("US");
+  const [callingCode, setCallingCode] = useState<string>("1"); 
+
+  useEffect(() => {
+    setIsDisabled(!(email.trim() && password.trim() && phoneno.trim() && checked));
+  }, [email, password, phoneno, checked]);
 
   const handleSignIn = (): void => {
-    if (email && password) {
-      Alert.alert("Sign In Successful", `Welcome, ${email}!`);
-    } else {
-      Alert.alert("Error", "Please enter both email and password.");
-    }
+    Alert.alert("Sign Up Successful", `Welcome, ${email}!`);
+    setEmail("");
+    setPassword("");
+    setPhoneno("");
+    setChecked(false);
+    setCountryCode("US"); 
+    setCallingCode("1");
   };
 
   return (
@@ -27,7 +38,7 @@ const SignUp: React.FC = () => {
       <Image source={require("../../assets/images/signup.png")} className="w-50 h-50 mx-auto"/>
 
       <TextInput
-        className="w-full h-15 text-white rounded-2xl px-4 mb-4 border border-gray-700"
+        className="w-full h-15 rounded-2xl px-4 mb-4 border border-gray-700 text-black"
         placeholder="Email"
         placeholderTextColor="#aaa"
         keyboardType="email-address"
@@ -36,17 +47,34 @@ const SignUp: React.FC = () => {
         onChangeText={setEmail}
       />
 
-      <TextInput
-        className="w-full h-15 text-white rounded-2xl px-4 mb-4 border border-gray-700"
-        placeholder="Password"
-        placeholderTextColor="#aaa"
-        secureTextEntry
-        value={password}
-        onChangeText={setPassword}
-      />
+<View className="flex-row items-center border border-gray-700 rounded-2xl px-4 mb-4">
+            {/* Country Picker */}
+            <CountryPicker
+              countryCode={countryCode || "US"}
+              withFlag
+              withCallingCode
+              withFilter
+              withModal
+              onSelect={(country: Country) => {
+                setCountryCode(country.cca2);
+                setCallingCode(country.callingCode[0]);
+              }}
+              visible={false}
+            />
+            <Text className="text-lg text-gray-800 ml-2">+{callingCode}</Text>
+
+            <TextInput
+              className="flex-1 h-12 pl-2 text-gray-800"
+              placeholder="Enter phone number"
+              placeholderTextColor="#aaa"
+              keyboardType="phone-pad"
+              value={phoneno}
+              onChangeText={setPhoneno}
+            />
+          </View>
 
 <TextInput
-        className="w-full h-15 text-white rounded-2xl px-4 mb-5 border border-gray-700"
+        className="w-full h-15 text-black rounded-2xl px-4 mb-5 border border-gray-700"
         placeholder="Password"
         placeholderTextColor="#aaa"
         secureTextEntry
@@ -73,9 +101,10 @@ const SignUp: React.FC = () => {
       </Text>
     </View>
     <TouchableOpacity
-        className="w-full h-12 rounded-2xl justify-center items-center mb-2"
-        style={{backgroundColor: "#0b2adb"}}
+        className = {`w-full h-12 rounded-2xl justify-center items-center mb-2 `}
+        style={{backgroundColor:isDisabled?'gray' : "#0b2adb"}}
         onPress={handleSignIn}
+        disabled={isDisabled} 
       >
         <Text className="text-white font-semibold text-lg">Sign Up</Text>
       </TouchableOpacity>
